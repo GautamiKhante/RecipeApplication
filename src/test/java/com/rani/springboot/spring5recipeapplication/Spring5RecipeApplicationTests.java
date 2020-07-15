da80b3da3da9729e8f0deeec74bbce9490b4bba6
Gautami Khante <33024174+GautamiKhante@users.noreply.github.com>
2020-07-16 01:50:47 +0530

    initial commit

---
+++ src/test/java/com/rani/springboot/spring5recipeapplication/Spring5RecipeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rani.springboot.spring5recipeapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
